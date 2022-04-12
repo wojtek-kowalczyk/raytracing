696d7d31d34681aad82d56a78f988e374765c1ed
wojtek-kowalczyk <wojtekkowalczyk01@gmail.com>
2022-04-12 19:51:21 +0200

    First image

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+
+    // image
+    const int width  = 256;
+    const int height = 256;
+
+    // render
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    // row-wise left->right then top->bottom
+    for (int j = height-1; j >= 0; --j)
+    {
+        for (int i = 0; i < width; ++i)
+        {
+            double r = static_cast<double>(i) / (width-1);
+            double g = static_cast<double>(j) / (height-1);
+            double b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n'; 
+            // run with: ./rt > file.ppm to save to file
+        }
+    }
+}
---
+++ makefile
@@ -0,0 +1,15 @@
+# List your *.h files here:
+headers = 
+
+# List your *.cpp files here:
+sources = main.cpp
+
+# Specify name of your program:
+executable = rt
+
+$(executable): $(headers) $(sources)
+	g++ -g -Wall -pedantic -std=c++20 $(sources) -o $(executable)
+
+.PHONY: clean
+clean:
+	rm $(executable)
